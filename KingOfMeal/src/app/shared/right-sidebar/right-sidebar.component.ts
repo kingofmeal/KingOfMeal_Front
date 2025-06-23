0c275d17160c2b5f0b7976307bf5423f20781fb7
sophie darbier <sophie.ambarian@gmail.com>
2025-06-23 15:48:15 +0200

    left sidebar Component

--- KingOfMeal/src/app/shared/right-sidebar/right-sidebar.component.ts
+++ KingOfMeal/src/app/shared/right-sidebar/right-sidebar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-right-sidebar',
-    imports: [],
-    templateUrl: './right-sidebar.component.html',
-    styleUrl: './right-sidebar.component.css'
+  selector: 'app-right-sidebar',
+  imports: [],
+  templateUrl: './right-sidebar.component.html',
+  standalone: true,
+  styleUrl: './right-sidebar.component.css'
 })
 export class RightSidebarComponent {
 
